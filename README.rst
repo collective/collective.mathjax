39ce296cc6d45c5eb9936f1d1c561f9c52b67464
Johannes Raggam <raggam-nl@adm.at>
2013-01-10 16:39:26 +0100

    initia; l

---
+++ README.rst
@@ -0,0 +1,17 @@
+Introduction
+============
+
+This package provides MathJax as browser resources for Plone and registers them
+in the Resource Registry.
+
+https://github.com/mathjax/MathJax
+
+http://www.mathjax.org/demos/tex-samples/
+
+http://en.wikipedia.org/wiki/MathJax
+
+
+Author
+======
+
+Johannes Raggam, BlueDynamics Alliance <raggam-nl [at] adm [dot] at>
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '1.0dev'
+
+setup(name='collective.mathjax',
+      version=version,
+      description="MathJax integration for Plone.",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.rst").read(),
+      classifiers=[
+          "Framework :: Plone",
+          "Programming Language :: Python",
+      ],
+      keywords='plone resources',
+      author='Johannes Raggam',
+      author_email='raggam-nl@adm.at',
+      url='http://github.com/collective/collective.mathjax',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
